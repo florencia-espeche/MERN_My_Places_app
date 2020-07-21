552366f5a47350625a74be1edda61088b9195fa7
Florencia Espeche <flor.espeche@gmail.com>
2020-07-21 20:15:41 -0300

    add menu components

--- client/src/user/pages/Users.js
+++ client/src/user/pages/Users.js
@@ -10,6 +10,13 @@ const Users = () => {
       image:
         'https://images.pexels.com/photos/1898555/pexels-photo-1898555.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
       places: 3
+    },
+    {
+      id: 'u2',
+      name: 'Regina',
+      image:
+        'https://images.pexels.com/photos/1377455/pexels-photo-1377455.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      places: 1
     }
   ];
 
