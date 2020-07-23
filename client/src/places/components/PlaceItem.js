d6913df3ce11ba238fbca95ebb66f8856e347207
Florencia Espeche <flor.espeche@gmail.com>
2020-07-22 23:31:30 -0300

    style for buttons

--- client/src/places/components/PlaceItem.js
+++ client/src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../../shared/components/UIElements/Card';
+import Button from '../../shared/components/FormElements/Button';
 
 import './PlaceItem.css';
 
@@ -16,8 +17,8 @@ const PlaceItem = (props) => {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <button>EDIT</button>
-          <button>DELETE</button>
+          <Button inverse to={`/places/${props.id}`}>EDIT</Button>
+          <Button danger>DELETE</Button>
         </div>
       </Card>
     </li>
