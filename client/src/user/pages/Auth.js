6e94bb7fc44e8f5eeba83fabead8f31af04e0703
Florencia Espeche <flor.espeche@gmail.com>
2020-11-08 13:04:52 -0300

    signup and login

---
+++ client/src/user/pages/Auth.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import Input from '../../shared/components/FormElements/Input';
+import Card from '../../shared/components/UIElements/Card';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hooks';
+import './Auth.css';
+import Button from '../../shared/components/FormElements/Button';
+
+const Auth = () => {
+  const [isLoguinMode, setIsLogiMode] = useState(true);
+  const [formState, inputHandler, setFormData] = useForm({
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  })
+
+  const switchModeHandler = () => {
+    if(!isLoguinMode){
+      setFormData(
+        {
+          ...formState.inputs,
+          name: undefined
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData({
+        ...formState.inputs,
+        name: {
+          value: '',
+          isValid: false
+        }
+      }, false);
+    }
+    setIsLogiMode(prevMode => !prevMode);
+  }
+
+  const authSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState.inputs);
+  }
+
+  return (
+    <Card className="auth">
+      <h2>Login Required</h2>
+      <hr />
+      <form onSubmit={authSubmitHandler}>
+        {!isLoguinMode && <Input element="input" id="name" type="text" label="Name"
+          validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a name." onInput={inputHandler}></Input>}
+        <Input element="input" id="email" type="email" label="E-mail"
+          validators={[VALIDATOR_EMAIL()]}
+          errorText="Please enter a valid email address."
+          onInput={inputHandler} />
+        <Input element="input" id="password" type="password" label="Password"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid password, at least 5 characters."
+          onInput={inputHandler} />
+        <Button type="submit" disabled={!formState.isValid}>
+          {isLoguinMode ? 'LOGIN' : 'SIGNUP'}
+        </Button>
+      </form>
+      <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoguinMode ? 'SIGNUP' : 'LOGIN'}</Button>
+    </Card>
+  );
+}
+
+export default Auth;
